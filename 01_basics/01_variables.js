d75ed13a708446f9c5dc697f8681f3e6d7461133
manivardhan1 <145485087+manivardhan1@users.noreply.github.com>
2025-12-04 20:10:23 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+ const accountId=1234
+ let accountEmail="manivardhan@gmail.com"
+ var accountPassword="12345"
+ accountCity="jaipur"
+ accoutEmail="mani@hc.com"
+ accountPassword="56676"
+ console.log(accountId)
+ console.table([accountId,accoutEmail,accountPassword,accountCity])
